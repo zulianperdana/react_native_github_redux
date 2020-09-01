4e80736ba672dd044cb242df952ee534b3cc9d93
zulian-uki <hiruma1997@gmail.com>
2020-09-01 22:25:33 +0700

    add commit redux module

---
+++ app/modules/commit/commit.action.reducer.ts
@@ -0,0 +1,46 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {CommitState, RepositoryItem} from 'schemas';
+
+const initialState: CommitState = {};
+
+const setRepository = (
+  state: CommitState,
+  repository: string,
+  change: (i: RepositoryItem) => RepositoryItem,
+) => {
+  const r: RepositoryItem = state[repository];
+  return {...state, [repository]: change(r)};
+};
+
+const commitSlice = createSlice({
+  name: 'commit',
+  initialState: initialState,
+  reducers: {
+    addRepository: (state, {payload}) => ({
+      ...state,
+      [payload.repository]: payload,
+    }),
+    setShowOnlyMyCommit: (state, {payload}) =>
+      setRepository(state, payload.repository, (r) => ({
+        ...r,
+        showOnlyMyCommit: payload.value,
+      })),
+    setCurrentPage: (state, {payload}) =>
+      setRepository(state, payload.repository, (r) => ({
+        ...r,
+        currentPage: payload.value,
+      })),
+    setPaginationDone: (state, {payload}) =>
+      setRepository(state, payload.repository, (r) => ({
+        ...r,
+        paginationDone: payload.value,
+      })),
+    setCommits: (state, {payload}) =>
+      setRepository(state, payload.repository, (r) => ({
+        ...r,
+        commits: payload.commits,
+      })),
+  },
+});
+
+export const {actions, reducer} = commitSlice;
---
+++ app/modules/commit/commit.thunk.ts
@@ -0,0 +1,75 @@
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import {CommitResults} from 'services/api/api.types';
+import {actions} from './commit.action.reducer';
+import {Api} from 'services/api/api';
+import {RepositoryItem} from 'schemas';
+
+export const loadRepository = createAsyncThunk(
+  'commit/loadRepository',
+  async (repository: string, {dispatch, extra}: any) => {
+    const api: Api = extra.api;
+    const defaultPerPage = 10;
+    const result: CommitResults = await api.getCommits(
+      repository,
+      false,
+      defaultPerPage,
+      1,
+    );
+    if (result.kind === 'ok') {
+      dispatch(
+        actions.addRepository({
+          commits: result.commits,
+          currentPage: 1,
+          paginationDone: false,
+          perPage: defaultPerPage,
+          repository,
+          showOnlyMyCommit: false,
+        }),
+      );
+      return true;
+    }
+    return false;
+  },
+);
+
+export interface LoadNextPageParam {
+  commit: RepositoryItem;
+  reset: boolean;
+}
+
+export const loadNextPage = createAsyncThunk(
+  'commit/loadNextPage',
+  async ({commit, reset}: LoadNextPageParam, {dispatch, extra}: any) => {
+    const api: Api = extra.api;
+    if (!commit.paginationDone || reset) {
+      const defaultPerPage = 10;
+      const {repository, perPage, showOnlyMyCommit, currentPage} = commit;
+      const result: CommitResults = await api.getCommits(
+        repository,
+        showOnlyMyCommit ?? false,
+        perPage ?? defaultPerPage,
+        reset ? 1 : currentPage + 1,
+      );
+      if (result.kind === 'ok') {
+        if (result.commits.length === 0 && !reset) {
+          dispatch(actions.setPaginationDone({repository, value: true}));
+        } else {
+          dispatch(
+            actions.setCurrentPage({repository, value: currentPage + 1}),
+          );
+          if (reset) {
+            dispatch(actions.setPaginationDone({repository, value: false}));
+            dispatch(actions.setCommits({repository, value: result.commits}));
+          } else {
+            dispatch(
+              actions.setCommits({
+                repository,
+                value: [...commit.commits, ...result.commits],
+              }),
+            );
+          }
+        }
+      }
+    }
+  },
+);
