439bb182945b1e69f3035f0d3b5026a871973239
zulian-uki <hiruma1997@gmail.com>
2020-09-02 21:17:23 +0700

    add commit list screen

---
+++ app/screens/commits.tsx
@@ -0,0 +1,164 @@
+import React, {PureComponent} from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {Header} from 'react-native-elements';
+import {connect} from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {BaseContainer, CheckBox, MenuActions, Text} from '@app/components';
+import CommitItemDisplay from '@app/components/commit-item';
+import {actions} from '@app/modules/commit/commit.action.reducer.ts';
+import repositorySelector from '@app/modules/commit/commit.selector';
+import {loadNextPage} from '@app/modules/commit/commit.thunk';
+import {RootState} from '@app/modules/store';
+import {CommitItem, RepositoryItem} from '@app/schemas';
+import {translate} from '@app/i18n';
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    marginTop: 24,
+  },
+  empty: {
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  footer: {
+    alignItems: 'center',
+    paddingBottom: 32,
+  },
+  headerView: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    height: 50,
+    marginLeft: 12,
+  },
+  headerText: {
+    flexGrow: 1,
+    flexShrink: 1,
+    marginLeft: 12,
+  },
+  listHeaderView: {
+    alignItems: 'flex-end',
+    marginBottom: 12,
+  },
+});
+
+class CommitScreen extends PureComponent {
+  renderTitle = (repository: string) => (
+    <Header
+      leftComponent={
+        <TouchableOpacity onPress={() => this.props.navigation.pop()}>
+          <Icon name={'arrow-back'} size={24} color="#000" />
+        </TouchableOpacity>
+      }
+      centerComponent={<Text>{repository}</Text>}
+      rightComponent={<MenuActions />}
+    />
+  );
+  renderCommit = (commit: CommitItem, repository: string) => (
+    <CommitItemDisplay
+      repository={repository}
+      key={commit.sha}
+      commit={commit}
+    />
+  );
+
+  onRefresh = async () => {
+    const {onLoad, repository} = this.props as any;
+    await onLoad(repository.repository, true);
+  };
+
+  onLoadMore = async (repository: RepositoryItem) => {
+    const {onLoad} = this.props as any;
+    await onLoad(repository.repository, false);
+  };
+  renderLoadMoreButton = () => {
+    const {repository} = this.props as any;
+    return (
+      <SafeAreaView style={styles.footer}>
+        {repository.commits.length > 0 &&
+          (repository.paginationDone ? (
+            <Text tx="commits.no_more_data" />
+          ) : repository.loadingMore ? (
+            <ActivityIndicator />
+          ) : (
+            <TouchableOpacity onPress={() => this.onLoadMore(repository)}>
+              <Text tx="commits.load_more" />
+            </TouchableOpacity>
+          ))}
+      </SafeAreaView>
+    );
+  };
+
+  renderListHeader = () => {
+    const {repository, onSetOnlyMyCommit} = this.props as any;
+    const {showOnlyMyCommit} = repository;
+    return (
+      <View style={styles.listHeaderView}>
+        <CheckBox
+          title={translate('commits.show_only_my_commits')}
+          checked={showOnlyMyCommit}
+          onPress={() => {
+            onSetOnlyMyCommit(!showOnlyMyCommit);
+            this.onRefresh(repository);
+          }}
+        />
+      </View>
+    );
+  };
+
+  renderEmptyCommit = () => (
+    <View style={styles.empty}>
+      <Text tx="commits.commits_not_found" />
+    </View>
+  );
+
+  render() {
+    const currentRepository = this.props.repository;
+    const {repository, refreshing, commits} = currentRepository;
+    return (
+      <BaseContainer withPadding={false}>
+        {this.renderTitle(repository)}
+        <FlatList
+          refreshing={refreshing}
+          onRefresh={this.onRefresh}
+          style={styles.content}
+          data={commits}
+          ListEmptyComponent={this.renderEmptyCommit()}
+          keyExtractor={(i) => i.sha}
+          ListFooterComponent={this.renderLoadMoreButton()}
+          ListHeaderComponent={this.renderListHeader()}
+          renderItem={(item) => this.renderCommit(item.item, repository)}
+        />
+      </BaseContainer>
+    );
+  }
+}
+
+const mapStateToProps = (state: RootState, props: any) => {
+  const repository = props.route.params.repository;
+  return {
+    repository: repositorySelector(state, {
+      repository: repository,
+    }),
+  };
+};
+
+const mapDispatchToProps = (dispatch: any, props: any) => {
+  const repository = props.route.params.repository;
+  return {
+    onLoad: (repositoryName: string, reset: boolean) =>
+      dispatch(loadNextPage({repositoryName, reset})),
+    onSetOnlyMyCommit: (value: boolean) =>
+      dispatch(actions.setShowOnlyMyCommit({value, repository})),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommitScreen);
--- app/screens/index.ts
+++ app/screens/index.ts
@@ -2,5 +2,6 @@ import LoginScreen from './login';
 import PasswordScreen from './password';
 import SplashScreen from './splash';
 import HomeScreen from './home';
+import CommitScreen from './commits';
 
-export {LoginScreen, SplashScreen, PasswordScreen, HomeScreen};
+export {LoginScreen, SplashScreen, PasswordScreen, HomeScreen, CommitScreen};
