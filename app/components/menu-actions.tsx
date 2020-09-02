107e188aae705bd9e196c2806952114ff191abf5
zulian-uki <hiruma1997@gmail.com>
2020-09-02 14:28:41 +0700

    add menu actions component

---
+++ app/components/menu-actions.tsx
@@ -0,0 +1,93 @@
+import React, {PureComponent} from 'react';
+import {BottomSheet, ListItem, withTheme, colors} from 'react-native-elements';
+import colorsDark from 'react-native-elements/src/config/colorsDark';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-native';
+import {compose} from '@reduxjs/toolkit';
+import {TouchableOpacity, TouchableWithoutFeedback} from 'react-native';
+
+import {RootState} from '@app/modules/store';
+import {toggleDarkMode} from '@app/modules/dark-mode/dark-mode.reducer';
+import {logout} from '@app/modules/user/user.thunk';
+import {translate} from '@app/i18n';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+class MenuActions extends PureComponent {
+  state = {
+    isVisible: false,
+  };
+
+  onClickLogout = async () => {
+    const {onLogout} = this.props as any;
+    await onLogout();
+  };
+
+  onClickChangeDarkMode = () => {
+    const {changeDarkMode, darkMode, updateTheme} = this.props as any;
+    changeDarkMode(!darkMode);
+    this.toggleBottomSheet();
+    updateTheme({colors: darkMode ? colors : colorsDark});
+  };
+
+  toggleBottomSheet = () => this.setState({isVisible: !this.state.isVisible});
+
+  renderOpenBottomSheetButton = () => (
+    <TouchableOpacity onPress={this.toggleBottomSheet}>
+      <Icon name="menu" size={24} />
+    </TouchableOpacity>
+  );
+
+  render() {
+    const {isVisible} = this.state;
+    const {darkMode} = this.props as any;
+    return (
+      <>
+        {this.renderOpenBottomSheetButton()}
+        <TouchableWithoutFeedback onPress={this.toggleBottomSheet}>
+          <BottomSheet
+            modalProps={{onRequestClose: this.toggleBottomSheet}}
+            isVisible={isVisible}>
+            <ListItem onPress={this.onClickChangeDarkMode}>
+              <ListItem.Content>
+                <ListItem.Title>
+                  {darkMode
+                    ? translate('settings.light_mode')
+                    : translate('settings.dark_mode')}
+                </ListItem.Title>
+              </ListItem.Content>
+            </ListItem>
+            <ListItem onPress={this.onClickLogout}>
+              <ListItem.Content>
+                <ListItem.Title>Logout</ListItem.Title>
+              </ListItem.Content>
+            </ListItem>
+            <ListItem onPress={this.toggleBottomSheet}>
+              <ListItem.Content>
+                <ListItem.Title>Close</ListItem.Title>
+              </ListItem.Content>
+            </ListItem>
+          </BottomSheet>
+        </TouchableWithoutFeedback>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state: RootState) => {
+  return {
+    darkMode: state.darkMode,
+  };
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    onLogout: () => dispatch(logout()),
+    changeDarkMode: (value: boolean) => dispatch(toggleDarkMode(value)),
+  };
+};
+
+export default compose(
+  withRouter,
+  withTheme,
+  connect(mapStateToProps, mapDispatchToProps),
+)(MenuActions);
